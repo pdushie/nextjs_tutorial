7962c868b350600ffb872d4305b2ee809f3e6c29
Philip Dushie <philip.dushie@gmail.com>
2025-03-04 13:23:41 -0400

    Fixed login issue

--- nextjs-dashboard/app/ui/login-form.tsx
+++ nextjs-dashboard/app/ui/login-form.tsx
@@ -19,7 +19,7 @@ export default function LoginForm() {
     undefined,
   );
   return (
-    <form className="space-y-3">
+    <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
